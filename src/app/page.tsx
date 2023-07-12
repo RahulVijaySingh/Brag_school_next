99b405f109e4dfd0ac505cb6f441e00370635f5a
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-07-12 16:52:27 +0530

    made changes in the footer and the image sliders of dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,131 +1,37 @@
-// import Image from 'next/image'
-
-// export default function Home() {
-//   return (
-//     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-//       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-//         <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-//           Get started by editing&nbsp;
-//           <code className="font-mono font-bold">src/app/page.tsx</code>
-//         </p>
-//         <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-//           <a
-//             className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{' '}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className="dark:invert"
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-//         <Image
-//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className="grid mb-32 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Docs{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Find in-depth information about Next.js features and API.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Learn{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Learn about Next.js in an interactive course with&nbsp;quizzes!
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Templates{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Explore the Next.js 13 playground.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Deploy{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   )
-// }
-
-
-
+"use client";
 import Image from 'next/image'
-import { Carousel } from 'flowbite';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import Head from 'next/head';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee, faFaceAngry } from '@fortawesome/free-solid-svg-icons';
+
 
 
 export default function Home() {
 
-  // for carousel
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 
 
@@ -265,20 +171,113 @@ export default function Home() {
 
       <h1 className='mt-5 text-3xl font-bold' style={{ textAlign: "center" }}>Live Competitions</h1>
 
-      <h5 className="text-xl" style={{ textAlign: "center" }}>Participate in different Events</h5>
+      <h5 className="mb-4 text-xl" style={{ textAlign: "center" }}>Participate in different Events</h5>
 
 
 
 
+{/* carousel */}
 
+<Carousel responsive={responsive}>
+  
+  <div className='m-2 card hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+    <img className='product--image'
+     src="https://i0.wp.com/schoolhues.com/wp-content/uploads/2023/05/Spelling-bee-2020-scaled-1.jpg?resize=2048%2C790&ssl=1" alt="carousel" />
+     <h1 className='mt-32 text-4xl font-bold '>Spelling Competition</h1>
+     <h1 className='font-bold'> August 2023</h1>
+  </div>
+ 
+  <div className='m-2 card hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+    <img className='product--image'
+     src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Horse_riding_in_coca_cola_arena_-_melbourne_show_2005.jpg/1200px-Horse_riding_in_coca_cola_arena_-_melbourne_show_2005.jpg" alt="carousel" />
+     <h1 className='text-4xl font-bold '>Horse Riding</h1>
+     <h1 className='font-bold'> August 2023</h1>
+  </div>
+ 
+  <div className='m-2 card hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+    <img className='product--image'
+     src="https://i0.wp.com/schoolhues.com/wp-content/uploads/2023/05/Chess_Tournaments_00-1024x585-1.webp?ssl=1" alt="carousel" />
+    <h1 className='mt-16 text-4xl font-bold '>Chess Tournament</h1>
+     <h1 className='font-bold'> August 2023</h1>
+  </div>
+ 
+  <div className='m-2 card hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+    <img className='product--image'
+     src="https://i0.wp.com/schoolhues.com/wp-content/uploads/2023/05/code-challenge-banner.png?ssl=1" alt="carousel" />
+     <h1 className='mt-32 text-4xl font-bold '>Kid CODING Challenge</h1>
+     <h1 className='font-bold'> August 2023</h1>
+  </div>
+ 
+  <div className='m-2 card hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+    <img className='product--image'
+     src="https://i0.wp.com/schoolhues.com/wp-content/uploads/2023/05/chess-1.jpg?ssl=1" alt="carousel" />
+    <h1 className='mt-16 text-3xl font-bold '>Chess FOR Seniors</h1>
+     <h1 className='font-bold'> August 2023</h1>
+  </div>
+ 
+  
+
+</Carousel>;
+
+
+
+<div className="mb-5" style={{ width: "100%", height: "400px", display: "flex", backgroundColor: "#999999", justifyContent: "center", flexDirection: "column", alignItems: "center" }} >
       
+      
+      <h3 className='font-bold'>CANDIDATE</h3>
+      <br />
+
+
+      <h1 style={{ fontSize: "64px" }}>Find your dream job</h1>
+      <h2 className='font-bold'>Recruiters looking for talent can find your resume based on</h2>
+      <h2 className='font-bold'>keywords, job titles, skills and more.</h2>
+
+     
+    </div> 
 
 
 
+{/* footer */}
+
+<footer className="mx-3" style={{ display: "grid", gridTemplateColumns: "30% auto auto 30%" }}>
+        <div className='mb-3 left-foot' style={{ display: "flex", flexDirection: "column", justifyContent: 'center' }} >
+          <h4>About us</h4>
+          <h5>SER Center, Prasadampadu, Vijayawada,</h5>
+          <h5>India - 521108</h5>
+          <h5>+91 9685511620</h5>
+          <h5>hello@schoolhues.com</h5>
+          <div >
+          <FontAwesomeIcon icon={faFaceAngry} />
+          <i className="mx-2 fa-brands fa-linkedin" ></i>
+          <i className="mx-2 fa-brands fa-twitter"></i>
+          <i className="mx-2 fa-brands fa-facebook-f"></i>
+          </div>      
+        </div>
+
+        <div className='mb-3 left-foot' style={{ display: "flex", flexDirection: "column", justifyContent: 'center' }} >
+          <h5>Company</h5>
+          <h5>About us</h5>
+          <h5>Blogs</h5>
+          <h5>Contacts</h5>
+        </div>
+
+        <div className='mb-3 left-foot' style={{ display: "flex", flexDirection: "column", justifyContent: 'center' }} >
+        <h5>Services</h5>
+        <h5>Jobs</h5>
+        <h5>Schools</h5>
+        <h5>Candidate</h5>
+        </div>
+
+        <div>
+        <img src="https://schoolhues.com/wp-content/uploads/2022/10/b.png" width="300" height="200" className="align-top d-inline-block" alt="" />
+        </div>
+
+      </footer>
 
 
-
-
+      <div className='container m-5' style={{margin:"100px"}}>
+      <h6>@ 2023 SchoolHues. All Right Reserved</h6>    
+       </div>
 
 
 
